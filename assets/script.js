252f648ae28e9ad0057c773493bc076c361727b0
Thunnder <gnorzea@gmail.com>
2022-04-23 15:26:28 -0400

    file basic sturcture

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon="+ lon + "&appid=d95b787e6646b2df5e52f029e7833998"
+
+var lat = 33.44
+var lon = 94.04
+
+console.log(apiUrl)

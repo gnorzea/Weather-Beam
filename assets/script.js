a3958606be0c48ccb80487c39cce137871daa1a9
Thunnder <gnorzea@gmail.com>
2022-04-24 20:34:09 -0400

    structural updates

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,26 @@
-var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon="+ lon + "&appid=d95b787e6646b2df5e52f029e7833998"
+var form = document.querySelector(".search-box form");
 
-var lat = 33.44
-var lon = 94.04
+var getSearch = function (search) {
 
-console.log(apiUrl)
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        var inputValue = event.target.value
+
+        var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?q=" + inputValue + "&appid=" + apiKey;
+
+        var apiKey = "120ea77b2ce7d183b863517d294c0544"
+
+        fetch(apiUrl).then(function (response) {
+
+            response.json().then(function (data) {
+                console.log(data);
+            })
+
+                .catch(function (error) {
+                    msg.textContent = "Please enter a valid city";
+                });
+        })
+    })
+};
+
+getSearch("boynton beach");
